969c127e2d587b4d0c026c7c1bf134e69e58a31f
Black-Male <Gkaranja20@gmail.com>
2022-11-21 12:25:03 +0300

    Classwork-1 Phase1 Javascripting

--- index.js
+++ index.js
@@ -6,39 +6,49 @@ function printValue(value){
 // 1.0 DEFINE VARIABLES
 //TODO: ADD fixed variable
 
-
+const beev = "churger"
+console.log(beev)
 //TODO: ADD redeclarable variable
+var score = "yo"
+console.log(score)
 
+var score = "mama"
+printValue(score)
 
 //TODO: ADD re-assignable but not redeclarable variable
-
+let age = 15
+printValue(age)
 
 // 2.0 DIFFERENT DATA TYPES
 //TODO: Create a fixed variable with a number value
-
+const ben =11
 
 //TODO: Create a redeclarable variable with a boolean value
-
+var m = false
 
 //TODO: Create a re-assignable variable with a string value
-
+let correctdate = "Monday"
 
 
 // 3.0 STRING OPERATIONS
 //TODO: Concatenate string (+)
+const message = 'I am now in phase1'
+const time = '12:00 PM'
 
-
+printValue(message+time)
 //TODO: Interpolate string (`${}`)
-
+const other = `${message} and the time is ${time}`
+printValue(other) 
 
 //TODO: Convert to uppercase
-
+const otherother = other.toUpperCase()
+printValue(otherother)
 
 //TODO: Convert to lowercase
-
+printValue(other.toLowerCase())
 
 //TODO: Index a specific character
-
+printValue("qwertyuiopasdfghjklzxcvbnm".charAt(11))
 
 //TODO: PRACTICE ON YOUR OWN (Check mozilla documentation)
 
